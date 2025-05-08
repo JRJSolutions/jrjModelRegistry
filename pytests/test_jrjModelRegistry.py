af16eb3f9ace10c91733616176c43c4d771bf153
Jason Jafari <me@jasonjafari.com>
2025-05-08 06:16:38 -0400

    chore: bump version to 0.0.11

--- pytests/test_jrjModelRegistry.py
+++ pytests/test_jrjModelRegistry.py
@@ -15,9 +15,23 @@ sys.path.insert(
 )
 
 
-from jrjModelRegistry import JrjMlModelRegistry
+from jrjModelRegistry import JrjMlModelRegistry, jrjRouterModelRegistry
 
 def test_test():
     jrjMlReg = JrjMlModelRegistry({})
     res = jrjMlReg.test(1)
     assert res == 1
+
+
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+
+app = FastAPI()
+app.include_router(jrjRouterModelRegistry)
+client = TestClient(app)
+
+
+def test_ping():
+    response = client.get("/jrjModelRegistry/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Welcome to JRJ Model Registry"}
